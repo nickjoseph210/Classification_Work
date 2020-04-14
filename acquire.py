642b9815d92b57ff73d519340757291b1948a335
Nick <nickpjoseph210@gmail.com>
2020-04-14 15:16:55 -0500

    first commit to classifications

---
+++ acquire.py
@@ -0,0 +1,29 @@
+# database connection
+
+from env import host, user, password
+
+def get_connection(db, user=user, host=host, password=password):
+    return f"mysql+pymysql://{user}:{password}@{host}/{db}"
+
+# getting titanic data as dataframe
+
+def get_titanic_data():
+    sql_query = "SELECT * FROM passengers"
+    return pd.read_sql(sql_query, get_connection("titanic_db"))
+
+# getting all the data from iris db
+
+def get_iris_data():
+    sql_query = """
+    SELECT species_id,
+    species_name,
+    sepal_length, 
+    sepal_width, 
+    petal_length,
+    petal_width
+    FROM measurements
+    JOIN species
+    USING (species_id)
+    """
+    return pd.read_sql(sql_query, get_connection("iris_db"))
+
